2ac97787dc69c5e8f5a7e3bc93016b3c71ce8dfe
rmdRagnarock <ermdmail@gmail.com>
2014-02-12 20:03:30 +0000

    Alert appears, timer loop ends

--- redPOP/ViewController.m
+++ redPOP/ViewController.m
@@ -20,7 +20,8 @@
 @implementation ViewController
 @synthesize button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, buttons, myCounterLabel, startButton, touch, swipe;
 
-bool notFinished = YES;
+int loopCount = 4;
+NSTimer * timer;
 int seconds,minutes, hours;
 int secondsLeft;
 UIAlertView*alert;
@@ -63,9 +64,13 @@ UIAlertView*alert;
 
 //Timer
 - (void)updateCounter:(NSTimer *)theTimer {
-    if(!secondsLeft == 0 && theTimer > 0){
+    
+    
+     if(!secondsLeft == 0 && theTimer > 0){
         startButton.hidden = YES;
+        
         secondsLeft -- ;
+         loopCount--;
         [self alternateColors];
         seconds = (secondsLeft %3600) % 60;
     }
@@ -76,8 +81,12 @@ UIAlertView*alert;
                     secondsLeft = 0;
                     
             }
-    if(secondsLeft  <= 0){
+    if(secondsLeft  <= 0)
+    {
+        [timer invalidate];
+        timer = nil;
         [alert show];
+        
     }
 }
 
@@ -85,17 +94,20 @@ UIAlertView*alert;
 -(void)countdownTimer{
     
     secondsLeft = seconds = 5;
-    
-        if([timer isValid]){
+
+     if([timer isValid]){
             stillValid = false;
         }
-        timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(updateCounter:) userInfo:nil repeats:YES];
+    
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(updateCounter:) userInfo:nil repeats:YES];
+    
 }
 
 -(void)callColors{
     {
         [self performSelector: @selector(alternateColors) withObject:nil afterDelay: 0.03];
-        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(alternateColors) userInfo:nil repeats:notFinished];
+        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(alternateColors) userInfo:nil repeats:YES];
         
     }
     return;
@@ -137,7 +149,8 @@ UIAlertView*alert;
 -(IBAction)pressedButton:(id)sender{
     score = 0;
     [self countdownTimer];
-    //[self callColors];
+    
+  
     
 }
 
